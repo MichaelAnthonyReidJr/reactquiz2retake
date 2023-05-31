39be61a50d47106a3b2d2ee42a91cbc1865ab286
Michael Reid <mreid@xpanxion.com>
2023-05-31 18:53:59 -0400

    Quiz

--- src/Buttons.jsx
+++ src/Buttons.jsx
@@ -1,12 +1,32 @@
 import React, {useState} from "react";
 
 export const Buttons = () => {
+    let[buttonType1, setButtonType1]=useState("Push")
+    let[buttonType2, setButtonType2]=useState("Push")
+    
+    const handleClick = (buttonType) => {
+        switch(buttonType) {
+        case (buttonType === 'B1'):
+            setButtonType1("Pushed");
+            break;
+        case (buttonType === 'B2'):
+            setButtonType2("Pushed");
+            break;
+        case (buttonType === 'Reset'):
+            setButtonType1("Push")
+            setButtonType2("Push")
+            break;
+         default:
+        break;
+
+        }
+    }
 
     return (
         <>
-            <button>Push</button>
-            <button>Push</button>
-            <button>Reset</button>
+            <button onClick= {()=> handleClick('B1') } >{buttonType1}</button>
+            <button onClick= {()=> handleClick('B2') } >{buttonType2}</button>
+            <button onClick= {()=> handleClick('Reset') }>Reset</button>
         </>
     )
 }
--- src/UpDownCounter.jsx
+++ src/UpDownCounter.jsx
@@ -1,13 +1,21 @@
 import React, {useState} from "react";
 
 export const UpDownCounter = () => {
-
+    let[counter, setCounter]=useState(0)
+    
+    const handleClick= (operation) => {
+        if (operation === 'Down'){
+            setCounter(--counter)
+        }else {
+            setCounter(++counter)
+        }
+        }
 
     return (
         <>
-            <button>Down</button>
-            {}
-            <button>Up</button>
+            <button onClick ={()=>handleClick('Down')}>Down</button>
+            {counter}
+            <button onClick ={()=>handleClick('Up')}>Up</button>
         </>
     )
 }
